55fcd37d6c62d82a31eca632d5c2f57c28306aaf
Udhaya0519 <udhaya0519@gmail.com>
2025-01-16 01:16:38 +0530

    finished layouts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,65 @@
+import { GrLocation } from 'react-icons/gr'
+import '../assets/css/contact.css'
+import { LuPhoneCall, LuShare2 } from 'react-icons/lu'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa6'
+import { MdOutlineMail } from 'react-icons/md'
+
 
 const Contact = () => {
   return (
-    <div>
-      Contact   
+    <div className="contact-section container">
+      <div className="contact-heading">
+        <span className="contact-sub-heading">
+          CONTACT
+        </span>
+        <h1>
+          CONTACT ME
+        </h1>
+      </div>
+      <div className="contact-grid">
+        <div className="contact-info">
+          <div className="location-icon-wrapper contact-icon-wrapper">
+            <GrLocation className='location-icon contact-info-icon'/>
+          </div>
+          <div className="location-info contact-sub-info">
+            <h5>My Address</h5>
+            <span>Chennai - 600053,IND</span>
+          </div>
+        </div>
+        <div className="contact-info">
+          <div className="social-icon-wrapper contact-icon-wrapper">
+            <LuShare2  className='social-icon contact-info-icon'/>
+          </div>
+          <div className="social-info contact-sub-info">
+            <h5>Social Profiles</h5>
+            <div className='contact-social-icons'>
+              <FaLinkedin />
+              <FaGithub />
+              <FaInstagram />
+              <FaTwitter />
+            </div>
+          </div>
+        </div>
+        <div className="contact-info">
+          <div className="mail-icon-wrapper contact-icon-wrapper">
+            <MdOutlineMail className='mail-icon contact-info-icon'/>
+          </div>
+          <div className="mail-info contact-sub-info">
+            <h5>Email Me</h5>
+            <span>udhaya0519@gmail.com</span>
+          </div>
+        </div>
+        <div className="contact-info">
+          <div className="call-icon-wrapper contact-icon-wrapper">
+            <LuPhoneCall className='call-icon contact-info-icon'/>
+          </div>
+          <div className="call-info contact-sub-info">
+            <h5>Call Me</h5>
+            <span>+91 9940302644</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,52 @@
+import { Link } from 'react-router-dom'
+import '../assets/css/projects.css'
+import { TbArrowBadgeRightFilled } from 'react-icons/tb'
+
+
 
 const Projects = () => {
   return (
-    <div>
-      projects
+    <div className='projects-section container'>
+      <div className="projects-heading">
+        <span className="projects-sub-heading">
+          PROJECTS
+        </span>
+        <h1>
+          MY WORKS
+        </h1>
+      </div>
+      <div className="project-repo-link">
+        <h4>For More Projects Visit</h4>
+        <TbArrowBadgeRightFilled className="right-cursor-icon"/>  
+        <Link to="https://github.com/Udhaya0519?tab=repositories">Github</Link>
+      </div>
+      <div className="project-flex">
+        <div className="project-item">
+          <img src="../public/images/project-image1.png" width="100%" height="100%" alt="" />
+          <div className='project-item-info'>
+            <div className="clip-border top"></div>
+            <div className='project-name'>Bridal Lash</div>
+            <div className="clip-border bottom"></div>
+          </div>
+        </div>
+        <div className="project-item">
+          <img src="../public/images/project-image1.png" width="100%" height="100%" alt="" />
+          <div className='project-item-info'>
+            <div className="clip-border top"></div>
+            <div className='project-name'>Bridal Lash</div>
+            <div className="clip-border bottom"></div>
+          </div>
+        </div>
+        <div className="project-item">
+          <img src="../public/images/project-image1.png" width="100%" height="100%" alt="" />
+          <div className='project-item-info'>
+            <div className="clip-border top"></div>
+            <div className='project-name'>Bridal Lash</div>
+            <div className="clip-border bottom"></div>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
